d41b1b8940c1f1b6006c8ab0ad8ef7f548acfe2e
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-02-27 12:34:05 -0600

    Relacion a dos entidades

--- WebApiEstadios/Controllers/EstadiosController.cs
+++ WebApiEstadios/Controllers/EstadiosController.cs
@@ -8,28 +8,73 @@ namespace WebApiEstadios.Controllers
     [Route("/estadios")]
     public class EstadiosController : ControllerBase
     {
-        private readonly ApplicationDbContext dbContext;
+        /*private readonly ApplicationDbContext dbContext;
 
         public EstadiosController(ApplicationDbContext dbContext)
         {
             this.dbContext = dbContext;
-        }
+        }*/
 
         [HttpGet]
         public ActionResult <List<Estadio>> Get()
         {
-            return new List<Estadio>() 
+            return new List<Estadio>()
             {
-                new Estadio{ Id = 1,Name="Estadio Universitario", Owner="UANL", Coords="San Nicolas de los Garza", ParkingCapacity=500, TotalSeats=41886},
-                new Estadio{ Id = 2,Name="Estadio BBVA", Owner = "FEMSA", Coords="Guadalupe", ParkingCapacity=200, TotalSeats=51348},
-                new Estadio{ Id = 3,Name="Estadio Azteca", Owner="Grupo Televisa", Coords="Ciudad de Mexico", ParkingCapacity=600, TotalSeats=87000}
+                new Estadio
+                {
+                    Id = 1,
+                    Name="Estadio Universitario",
+                    Owner="UANL",
+                    Coords="San Nicolas de los Garza",
+                    ParkingCapacity=500,
+                    TotalSeats=41886,
+                    Areas = new List<Area>()
+                    {
+                        new Area { Id = 1, Name="VIP", Capacity=100, Available=70, Taken=20 },
+                        new Area { Id = 2,Name="East", Capacity=500, Available=120, Taken=380 },
+                        new Area { Id = 3,Name="Staff", Capacity=150, Available=20, Taken=130 }
+                    },
+                    EstadioType = new EstadioType { Id = 1, Type = "Fútbol" }
+                },
+                new Estadio
+                { 
+                    Id = 2,
+                    Name="Estadio BBVA", 
+                    Owner = "FEMSA", 
+                    Coords="Guadalupe", 
+                    ParkingCapacity=200, 
+                    TotalSeats=51348,
+                    Areas = new List<Area>()
+                    {
+                        new Area { Id = 1, Name="VIP", Capacity=100, Available=70, Taken=20 },
+                        new Area { Id = 2,Name="East", Capacity=500, Available=120, Taken=380 },
+                        new Area { Id = 3,Name="Staff", Capacity=150, Available=20, Taken=130 }
+                    },
+                    EstadioType = new EstadioType { Id = 1, Type = "Fútbol" }
+                },
+                new Estadio
+                { 
+                    Id = 3,
+                    Name="Estadio Azteca", 
+                    Owner="Grupo Televisa", 
+                    Coords="Ciudad de Mexico", 
+                    ParkingCapacity=600, 
+                    TotalSeats=87000,
+                    Areas = new List<Area>()
+                    {
+                        new Area { Id = 1, Name="VIP", Capacity=100, Available=70, Taken=20 },
+                        new Area { Id = 2,Name="East", Capacity=500, Available=120, Taken=380 },
+                        new Area { Id = 3,Name="Staff", Capacity=150, Available=20, Taken=130 }
+                    },
+                    EstadioType = new EstadioType { Id = 1, Type = "Fútbol" }
+                }
             };
         }
 
-        [HttpGet("first")]
+        /*[HttpGet("first")]
         public async Task<ActionResult<Estadio>> FirstEstadium()
         {
             return await dbContext.Estadios.FirstOrDefaultAsync();
-        }
+        }*/
     }
 }
