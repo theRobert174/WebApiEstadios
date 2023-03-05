f3e72367ead41eea030e9a82bba381d665a58f87
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-03-04 20:18:49 -0600

    Regresion a lo basico

--- WebApiEstadios/Controllers/EstadiosController.cs
+++ WebApiEstadios/Controllers/EstadiosController.cs
@@ -8,12 +8,6 @@ namespace WebApiEstadios.Controllers
     [Route("/estadios")]
     public class EstadiosController : ControllerBase
     {
-        /*private readonly ApplicationDbContext dbContext;
-
-        public EstadiosController(ApplicationDbContext dbContext)
-        {
-            this.dbContext = dbContext;
-        }*/
 
         [HttpGet]
         public ActionResult <List<Estadio>> Get()
@@ -27,14 +21,7 @@ namespace WebApiEstadios.Controllers
                     Owner="UANL",
                     Coords="San Nicolas de los Garza",
                     ParkingCapacity=500,
-                    TotalSeats=41886,
-                    Areas = new List<Area>()
-                    {
-                        new Area { Id = 1, Name="VIP", Capacity=100, Available=70, Taken=20 },
-                        new Area { Id = 2,Name="East", Capacity=500, Available=120, Taken=380 },
-                        new Area { Id = 3,Name="Staff", Capacity=150, Available=20, Taken=130 }
-                    },
-                    EstadioType = new EstadioType { Id = 1, Type = "Fútbol" }
+                    TotalSeats=41886
                 },
                 new Estadio
                 { 
@@ -43,14 +30,7 @@ namespace WebApiEstadios.Controllers
                     Owner = "FEMSA", 
                     Coords="Guadalupe", 
                     ParkingCapacity=200, 
-                    TotalSeats=51348,
-                    Areas = new List<Area>()
-                    {
-                        new Area { Id = 1, Name="VIP", Capacity=100, Available=70, Taken=20 },
-                        new Area { Id = 2,Name="East", Capacity=500, Available=120, Taken=380 },
-                        new Area { Id = 3,Name="Staff", Capacity=150, Available=20, Taken=130 }
-                    },
-                    EstadioType = new EstadioType { Id = 1, Type = "Fútbol" }
+                    TotalSeats=51348
                 },
                 new Estadio
                 { 
@@ -59,22 +39,9 @@ namespace WebApiEstadios.Controllers
                     Owner="Grupo Televisa", 
                     Coords="Ciudad de Mexico", 
                     ParkingCapacity=600, 
-                    TotalSeats=87000,
-                    Areas = new List<Area>()
-                    {
-                        new Area { Id = 1, Name="VIP", Capacity=100, Available=70, Taken=20 },
-                        new Area { Id = 2,Name="East", Capacity=500, Available=120, Taken=380 },
-                        new Area { Id = 3,Name="Staff", Capacity=150, Available=20, Taken=130 }
-                    },
-                    EstadioType = new EstadioType { Id = 1, Type = "Fútbol" }
+                    TotalSeats=87000
                 }
             };
         }
-
-        /*[HttpGet("first")]
-        public async Task<ActionResult<Estadio>> FirstEstadium()
-        {
-            return await dbContext.Estadios.FirstOrDefaultAsync();
-        }*/
     }
 }
