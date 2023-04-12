63af3d4ac2a1ca7a32755d4d835d1b8d2aff33e6
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-04-12 14:48:18 -0600

    validaciones personalizadas

--- WebApiEstadios/Entidades/Estadio.cs
+++ WebApiEstadios/Entidades/Estadio.cs
@@ -1,16 +1,19 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using WebApiEstadios.Validaciones;
 
 namespace WebApiEstadios.Entidades
 {
-    public class Estadio
+    public class Estadio : IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "Ingrese el nombre({0}) del estadio")]
         [StringLength(maximumLength: 50,MinimumLength = 4, ErrorMessage = "Reduzca el nombre o mejor use siglas")]
+        //[firstCharCapital]
         public string Name { get; set;}
 
         [Required(ErrorMessage = "Ingrese el propietario({0}) del estadio")]
         [StringLength(maximumLength: 50, MinimumLength = 4, ErrorMessage = "Reduzca el nombre o mejor use siglas")]
+        [firstCharCapital]
         public string Owner { get; set;}
 
         [Required(ErrorMessage = "Ingrese la localizacion({0}) del estadio")]
@@ -27,5 +30,19 @@ namespace WebApiEstadios.Entidades
 
         public List<Area> Areas { get; set;}
         //public EstadioType EstadioType { get; set; }
+
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(!string.IsNullOrEmpty(Name))
+            {
+                var firstChar = Name[0].ToString();
+
+                if(firstChar != firstChar.ToUpper())
+                {
+                    yield return new ValidationResult("El nombre{0} debe iniciar con mayuscula", new String[] { nameof(Name) });
+                }
+            }
+        }
     }
 }
